31937a14a08979548b4a0373e67467fa0ada6960
Christian Forssén <christian.forssen@chalmers.se>
2019-08-15 10:07:52 +0200

    added Canvas URL:s

--- doc/web/courseURLs.js
+++ doc/web/courseURLs.js
@@ -1,4 +1,4 @@
-var material = "https://chalmers.instructure.com/courses/7773/external_tools/214";
+var material = "https://chalmers.instructure.com/courses/7773/external_tools/221";
 var syllabus = "https://chalmers.instructure.com/courses/7773/assignments/syllabus";
-var schedule = "https://chalmers.instructure.com/courses/7773/external_tools/212";
+var schedule = "https://chalmers.instructure.com/courses/7773/external_tools/223";
 var project1 = "https://chalmers.instructure.com/courses/7773/assignments/4895";
