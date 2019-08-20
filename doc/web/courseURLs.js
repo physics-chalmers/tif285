778b80c1399898a0070cd84692774cfe7d8315ed
Christian Forssén <christian.forssen@chalmers.se>
2019-08-20 10:17:40 +0200

    javascript tests; URL host dependent links

--- doc/web/courseURLs.js
+++ doc/web/courseURLs.js
@@ -1,3 +1,25 @@
+var protocolURL = window.location.protocol
+var hostURL = window.location.host
+var pathURL = window.location.pathname
+var pathArray = window.location.pathname.split('/');
+var secondLevelLocation = pathArray[0];
+var lastLevelLocation = pathArray[pathArray.length-1];
+var oldURL = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname + window.location.search
+
+// Change filename if it is in the URL
+if (lastLevelLocation.includes(".html")) {
+	pathArray[pathArray.length-1] = 'material.html';
+}
+
+var newPathname = "";
+for (i = 0; i < pathArray.length; i++) {
+  newPathname += "/";
+  newPathname += pathArray[i];
+}
+
+var newURL = window.location.protocol + "//" + window.location.host + "/" + newPathname + window.location.search
+
+
 var material = "https://chalmers.instructure.com/courses/7773/external_tools/221";
 var syllabus = "https://chalmers.instructure.com/courses/7773/assignments/syllabus";
 var schedule = "https://chalmers.instructure.com/courses/7773/external_tools/223";
