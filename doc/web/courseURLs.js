59c3ee4af7bdc5ebd003fee4afc6fe1c5c0893ab
Christian Forssén <christian.forssen@chalmers.se>
2019-08-20 10:29:45 +0200

    try java script in Canvas setting.

--- doc/web/courseURLs.js
+++ doc/web/courseURLs.js
@@ -1,6 +1,8 @@
+var pathURL = window.location.pathname
+
+/*
 var protocolURL = window.location.protocol
 var hostURL = window.location.host
-var pathURL = window.location.pathname
 var pathArray = window.location.pathname.split('/');
 var secondLevelLocation = pathArray[0];
 var lastLevelLocation = pathArray[pathArray.length-1];
@@ -17,8 +19,19 @@ for (i = 0; i < pathArray.length; i++) {
   newPathname += pathArray[i];
 }
 
-var newURL = window.location.protocol + "//" + window.location.host + "/" + newPathname + window.location.search
+// var newURL = window.location.protocol + "//" + window.location.host + "/" + newPathname + window.location.search
+*/
 
+// course-specific URL
+var courseURL = "https://chalmers.instructure.com/courses/7773/";
+
+// Relative link to file if the current filename is in the URL
+var newURL = "";
+if (pathURL.includes(".html")) {
+	newURL = 'material.html';
+} else {
+	newURL = courseURL + "external_tools/221";
+}
 
 var material = "https://chalmers.instructure.com/courses/7773/external_tools/221";
 var syllabus = "https://chalmers.instructure.com/courses/7773/assignments/syllabus";
