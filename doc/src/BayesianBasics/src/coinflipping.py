e1127d5f464877ad5e65dc7dad577e4d1ba24fa6
Christian Forssén <christian.forssen@chalmers.se>
2019-09-10 08:18:05 +0200

    added summary of pdfs

--- doc/src/BayesianBasics/src/coinflipping.py
+++ doc/src/BayesianBasics/src/coinflipping.py
@@ -15,7 +15,7 @@ heads=flips<pH              # boolean array, heads[i]=True if flip i is heads
 # start bayesian setup
 def prior(pH):
     p=np.zeros_like(pH)
-    p[(0<=x)&(x<=1)]=1      # allowed range: 0<=pH<=1
+    p[(0<=pH)&(pH<=1)]=1      # allowed range: 0<=pH<=1
     return p                # uniform prior
 def likelihood(pH,data):
     N = len(data)
@@ -29,13 +29,13 @@ def posterior(pH,data):
 # end bayesian setup
 
 # start plotting
-x=np.linspace(0,1,1000)
+pH=np.linspace(0,1,1000)
 fig, axs = plt.subplots(nrows=4,ncols=3,sharex=True,sharey='row',figsize=(14,14))
 axs_vec=np.reshape(axs,-1)
-axs_vec[0].plot(x,prior(x))
+axs_vec[0].plot(pH,prior(pH))
 for ndouble in range(11):
     ax=axs_vec[1+ndouble]
-    ax.plot(x,posterior(x,heads[:2**ndouble]))
+    ax.plot(pH,posterior(pH,heads[:2**ndouble]))
     ax.text(0.1, 0.8, '$N={0}$'.format(2**ndouble), transform=ax.transAxes)
 for row in range(4): axs[row,0].set_ylabel('$p(p_H|D_\mathrm{obs},I)$')
 for col in range(3): axs[-1,col].set_xlabel('$p_H$')
