1f5abe18aca18b751cb10067931aac73981704ba
Christian Forssén <christian.forssen@chalmers.se>
2021-05-10 14:50:24 +0200

    first complete version of jupyter-book files. Includes the _build directory

---
+++ doc/LectureNotes/_build/jupyter_execute/demo-MCMC.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Metropolis-Hasting MCMC sampling of a Poisson distribution 
+# 
+# Last revised: 20-Sep-2020 by Christian Forssén [christian.forssen@chalmers.se]
+
+# This notebook was adapted from Example 1, section 12.2 in Gregory's *Bayesian Logical Data Analysis for the Physical Sciences*.
+
+# The Poisson discrete random variable from scipy.stats is defined by (see [documentation](https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.poisson.html))
+# 
+# $$
+# p(k|\mu) = \frac{\mu^k e^{-\mu}}{k!} \quad \mbox{for }k\geq 0 \;.
+# $$
+# 
+# where $k$ is an integer and $\mu$ is called the shape parameter. The mean and variance of this distribution are both equal to $\mu$. (Note: Gregory uses a different notation, but we'll stay consistent with scipy.stats.)
+
+# By "sampling a Poisson distribution" we mean we will obtain a set of $k$ values: $\{k_1, k_2, k_3, \ldots\}$ that follow this distribution.  That is, for a particular $k_i$, the probability to get that value should be what we get by evaluating $p(k_i|\mu)$. We know we've succeeded if we make a histogram of our set of $k_i$s and it looks like $p(k|\mu)$ (scaled to line up or else our histogram needs to be normalized to one).
+# 
+# The method we'll use is generically called Markov chain Monte Carlo or MCMC.  A Markov chain starts with some initial value, and then each successive value is generated from the previous one.  But it is not deterministic: when the new value is chosen, there is a random number involved.  The particular version of MCMC used here is called Metropolis-Hasting. You may be familiar with this from a statistical mechanics class, where it is typically applied to the Ising model.
+
+# We'll do the Metropolis-Hasting sampling as follows:
+# 1. Choose an initial $k$ (call it $k_0$), having already fixed $\mu$.
+# 2. Given $k_i$, sample a uniform random number $x$ from 0 to 1 (so $x \sim U(0,1)$) and propose $k' = k_i + 1$ if the $x > 0.5$, otherwise propose $k' = k_i - 1$.
+# 3. Compute the Metropolis ratio $r = p(k'|\mu)\, /\, p(k_i|\mu)$ using the discrete Poisson distribution.
+# 4. Given another uniform random number $y \sim U(0,1)$, $k_{i+1} = k'$ if $y \leq r$, else $k_{i+1} = k_i$ (i.e., keep the same value for the next $k$).
+# 5. Repeat 2.-4. until you think you have enough samples of $k$.
+# 6. When graphing the posterior or calculating averages, skip the first values until the sampling has equilibrated (this is generally called the "burn-in" or "warm-up").
+# 
+# In practice we'll carry this out by generating all our uniform random numbers at the beginning using `scipy.stats.uniform.rvs`.
+
+# In[1]:
+
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+import numpy as np
+from math import factorial
+
+# We'll get our uniform distributions from stats, but there are other ways.
+import scipy.stats as stats  
+
+import matplotlib.pyplot as plt
+import seaborn; seaborn.set() # for nicer plot formatting
+
+
+# In[2]:
+
+
+def poisson(k, mu):
+    """
+    Returns a Poisson distribution value for k with mean mu
+    """
+    return mu**k * np.exp(-mu) / factorial(k) 
+
+
+# In the following we have the steps 1-6 defined above marked in the code. *Step through the implementation and ask questions about what you don't understand.*
+
+# In[3]:
+
+
+# 1. Set mu and k0
+mu = 3.
+k0 = 10.
+
+num_steps = 1000  # number of MCMC steps we'll take
+# generate the two sets of uniform random numbers we'll need for 2. and 4.
+uniform_1 = stats.uniform.rvs(size=num_steps)  
+uniform_2 = stats.uniform.rvs(size=num_steps)
+
+k_array = np.zeros(num_steps, dtype=int)
+k_array[0] = k0
+
+
+# In[4]:
+
+
+# 5. Loop through steps 2-4
+for i in range(num_steps-1):  # num_steps-1 so k_array[i+1] is always defined
+    # 2. Propose a step
+    k_now = k_array[i]
+    if uniform_1[i] > 0.5:
+        kp = k_now + 1          # step to the right
+    else:
+        kp = max(0, k_now - 1)  # step to the left, but don't go below zero
+    
+    # 3. Calculate Metropolis ratio
+    metropolis_r = poisson(kp, mu) / poisson(k_now, mu)
+    # 4. Accept or reject
+    if uniform_2[i] <= metropolis_r:
+        k_array[i+1] = kp
+    else:
+        k_array[i+1] = k_now
+
+
+# In[5]:
+
+
+# 6. Choose how many steps to skip
+warm_up_steps = 100
+
+fig = plt.figure(figsize=(10,10))
+
+# Plot the trace (that means k_i for i=0 to num_steps)
+ax_trace = fig.add_subplot(1, 1,1)    
+ax_trace.plot(range(num_steps), k_array, color='blue')
+ax_trace.set_ylim(0, 12)
+ax_trace.set_xlabel(r'$i$')
+ax_trace.set_ylabel(r'$k_i$')
+trace_title = rf'MCMC trace for Poisson distribution with $\mu = ${mu:.1f}'
+ax_trace.set_title(trace_title);
+
+
+# In[6]:
+
+
+fig = plt.figure(figsize=(10,10))
+# Plot the Poisson distribution
+ax_plot = fig.add_subplot(1, 1, 1)
+bin_num = 12
+n_pts = range(bin_num)
+    
+# Scale exact result to the histogram, accounting for warm_up_steps    
+poisson_pts = [(num_steps - warm_up_steps) * poisson(n, mu) for n in n_pts]     
+    
+# Plot the exact distribution    
+ax_plot.plot(n_pts, poisson_pts, marker='o', color='black') 
+# Histogram k_i beyond the warm-up period
+ax_plot.hist(k_array[warm_up_steps:], bins=n_pts,
+             align='left', rwidth=0.2, color='red')
+ax_plot.set_xlim(-1, bin_num)
+ax_plot.set_xlabel(r'$k$')
+plot_title = rf'$\mu = ${mu:.1f}  # steps = {num_steps:d},'                + ' # warm-up steps = {warm_up_steps:d}'
+ax_plot.set_title(plot_title)
+
+fig.tight_layout()
+
+
+# *What do you observe about these plots?*
+
+# In[7]:
+
+
+# Check the mean and standard deviations from the samples against exac
+print(f' MCMC mean = {np.mean(k_array[warm_up_steps:]):.2f}')
+print(f'Exact mean = {stats.poisson.mean(mu=mu):.2f}')
+print(f'   MCMC sd = {np.std(k_array[warm_up_steps:]):.2f}')
+print(f'  Exact sd = {stats.poisson.std(mu=mu):.2f}')      
+
+
+# * *How do you expect the accuracy of the estimate of the mean scales with the number of points? How would you test it?* 
+# 
+# * *Record values of the MCMC mean for `num_steps` = 1000, 4000, and 16000, running each 10-20 times.  Explain what you find.* 
+# 
+# * *Calculate the mean and standard deviations of the means you found (using `np.mean()` and `np.std()`.  Explain your results.*
+# 
+# * *Predict what you will find from 10 runs at `num_steps` = 100,000.  What did you actually find?*
+
+# In[8]:
+
+
+# Sample results with 1000, 4000, 16000, 100000 
+# These are recorded results. Try to extract your own series.
+
+mean1000 = np.array([3.18, 2.83, 3.45, 2.78, 2.51, 2.81, 2.99, 3.29, 3.19, 3.46])
+print(f'1000 [10]:  {np.mean(mean1000):.2f} +/- {np.std(mean1000):.2f}')
+mean1000b = np.array([2.86, 3.20, 3.13, 2.79, 3.04, 3.17, 2.93, 2.95, 2.80, 2.73])
+print(f'1000 [10]:  {np.mean(mean1000b):.2f} +/- {np.std(mean1000b):.2f}')
+mean1000c = np.concatenate([mean1000, mean1000b])
+print(f'1000 [20]:  {np.mean(mean1000c):.2f} +/- {np.std(mean1000c):.2f}')
+
+print(' ')
+
+mean4000 = np.array([3.05, 3.07, 2.90, 3.10, 3.04, 3.02, 3.06, 3.04, 2.99, 3.18])
+print(f'4000 [10]:  {np.mean(mean4000):.2f} +/- {np.std(mean4000):.2f}')
+mean4000b = np.array([3.04, 2.91, 3.02, 3.00, 2.87, 2.92, 2.76, 3.17, 3.01, 3.04])
+print(f'4000 [10]:  {np.mean(mean4000b):.2f} +/- {np.std(mean4000b):.2f}')
+mean4000c = np.concatenate([mean4000, mean4000b])
+print(f'4000 [20]:  {np.mean(mean4000c):.2f} +/- {np.std(mean4000c):.2f}')
+
+print(' ')
+
+mean16000 = np.array([3.08, 3.01, 3.04, 2.93, 3.11, 2.98, 3.05, 3.12, 3.04, 3.08])
+print(f'16000 [10]:  {np.mean(mean16000):.2f} +/- {np.std(mean16000):.2f}')
+mean16000b = np.array([3.05, 2.98, 2.99, 2.89, 3.04, 3.11, 2.90, 2.92, 2.94, 2.98])
+print(f'16000 [10]:  {np.mean(mean16000b):.2f} +/- {np.std(mean16000b):.2f}')
+mean16000c = np.concatenate([mean16000, mean16000b])
+print(f'16000 [20]:  {np.mean(mean16000c):.2f} +/- {np.std(mean16000c):.2f}')
+
+print(' ')
+
+mean100000 = np.array([3.00, 3.02, 2.95, 3.01, 3.00, 3.00, 2.99, 3.00, 2.94, 2.99])
+print(f'100000 [10]:  {np.mean(mean100000):.3f} +/- {np.std(mean100000):.3f}')
+mean100000b = np.array([3.03, 3.01, 3.01, 3.01, 2.99, 3.01, 2.99, 3.01, 2.99, 3.00])
+print(f'100000 [10]:  {np.mean(mean100000b):.3f} +/- {np.std(mean100000b):.3f}')
+mean100000c = np.concatenate([mean100000, mean100000b])
+print(f'100000 [20]:  {np.mean(mean100000c):.3f} +/- {np.std(mean100000c):.3f}')
+
+# Should have kept 3 digits for 100000 runs
+# Basically we see the 1/np.sqrt(N) improvement in the standard deviation
+#  of the predictions for the mean.
+
+
+# In[9]:
+
+
+fig,ax = plt.subplots(1,1)
+nsamples = np.array([1000,4000,16000,100000])
+stds = np.array([np.std(mean1000c),np.std(mean4000c),np.std(mean16000c),np.std(mean100000c)])
+ax.semilogx(nsamples,1/np.sqrt(nsamples)*0.25*np.sqrt(1000),label=r'$1/\sqrt{N}$')
+ax.semilogx(nsamples,stds,'o',label=r'$\sigma(N)$')
+ax.set_xlabel(r'$N$')
+ax.set_ylabel(r'$\sigma$')
+ax.legend(loc='best');
+
+
+# In[ ]:
+
+
+
+
---
+++ doc/LectureNotes/_build/jupyter_execute/demo-cnn.py
@@ -0,0 +1,326 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Convolutional Neural Network 
+# ## Image recognition demonstration
+# 
+# Last revised: 20-Oct-2019 by Christian Forssén [christian.forssen@chalmers.se]
+
+# ##### Copyright 2019 The TensorFlow Authors.
+
+# In[1]:
+
+
+#@title Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# # Convolutional Neural Network (CNN)
+
+# <table class="tfo-notebook-buttons" align="left">
+#   <td>
+#     <a target="_blank" href="https://www.tensorflow.org/tutorials/images/cnn">
+#     <img src="https://www.tensorflow.org/images/tf_logo_32px.png" />
+#     View on TensorFlow.org</a>
+#   </td>
+#   <td>
+#     <a target="_blank" href="https://colab.research.google.com/github/tensorflow/docs/blob/master/site/en/tutorials/images/cnn.ipynb">
+#     <img src="https://www.tensorflow.org/images/colab_logo_32px.png" />
+#     Run in Google Colab</a>
+#   </td>
+#   <td>
+#     <a target="_blank" href="https://github.com/tensorflow/docs/blob/master/site/en/tutorials/images/cnn.ipynb">
+#     <img src="https://www.tensorflow.org/images/GitHub-Mark-32px.png" />
+#     View source on GitHub</a>
+#   </td>
+#   <td>
+#     <a href="https://storage.googleapis.com/tensorflow_docs/docs/site/en/tutorials/images/cnn.ipynb"><img src="https://www.tensorflow.org/images/download_logo_32px.png" />Download notebook</a>
+#   </td>
+# </table>
+
+# This tutorial demonstrates training a simple [Convolutional Neural Network](https://developers.google.com/machine-learning/glossary/#convolutional_neural_network) (CNN) to classify [CIFAR images](https://www.cs.toronto.edu/~kriz/cifar.html). Because this tutorial uses the [Keras Sequential API](https://www.tensorflow.org/guide/keras/overview), creating and training our model will take just a few lines of code.
+# 
+
+# ### Import TensorFlow
+
+# In[2]:
+
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+try:
+  # %tensorflow_version only exists in Colab.
+  get_ipython().run_line_magic('tensorflow_version', '2.x')
+except Exception:
+  pass
+import tensorflow as tf
+
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+
+
+# ### Download and prepare the CIFAR10 dataset
+# 
+# 
+# The CIFAR10 dataset contains 60,000 color images in 10 classes, with 6,000 images in each class. The dataset is divided into 50,000 training images and 10,000 testing images. The classes are mutually exclusive and there is no overlap between them.
+
+# In[3]:
+
+
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+
+# Normalize pixel values to be between 0 and 1
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+
+# ### Verify the data
+# 
+# To verify that the dataset looks correct, let's plot the first 25 images from the training set and display the class name below each image.
+# 
+
+# In[4]:
+
+
+class_names = ['airplane', 'automobile', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck']
+
+plt.figure(figsize=(10,10))
+for i in range(25):
+    plt.subplot(5,5,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    # The CIFAR labels happen to be arrays, 
+    # which is why you need the extra index
+    plt.xlabel(class_names[train_labels[i][0]])
+plt.show()
+
+
+# ### Create the convolutional base
+
+# The 6 lines of code below define the convolutional base using a common pattern: a stack of [Conv2D](https://www.tensorflow.org/api_docs/python/tf/keras/layers/Conv2D) and [MaxPooling2D](https://www.tensorflow.org/api_docs/python/tf/keras/layers/MaxPool2D) layers.
+# 
+# As input, a CNN takes tensors of shape (image_height, image_width, color_channels), ignoring the batch size. If you are new to these dimensions, color_channels refers to (R,G,B). In this example, you will configure our CNN to process inputs of shape (32, 32, 3), which is the format of CIFAR images. You can do this by passing the argument `input_shape` to our first layer.
+# 
+# 
+
+# In[5]:
+
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+
+# Let's display the architecture of our model so far.
+
+# In[6]:
+
+
+model.summary()
+
+
+# Above, you can see that the output of every Conv2D and MaxPooling2D layer is a 3D tensor of shape (height, width, channels). The width and height dimensions tend to shrink as you go deeper in the network. The number of output channels for each Conv2D layer is controlled by the first argument (e.g., 32 or 64). Typically,  as the width and height shrink, you can afford (computationally) to add more output channels in each Conv2D layer.
+
+# ### Add Dense layers on top
+# To complete our model, you will feed the last output tensor from the convolutional base (of shape (3, 3, 64)) into one or more Dense layers to perform classification. Dense layers take vectors as input (which are 1D), while the current output is a 3D tensor. First, you will flatten (or unroll) the 3D output to 1D,  then add one or more Dense layers on top. CIFAR has 10 output classes, so you use a final Dense layer with 10 outputs and a softmax activation.
+
+# In[7]:
+
+
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+
+# Here's the complete architecture of our model.
+
+# In[8]:
+
+
+model.summary()
+
+
+# As you can see, our (3, 3, 64) outputs were flattened into vectors of shape (576) before going through two Dense layers.
+
+# ### Compile and train the model
+
+# In[9]:
+
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+history = model.fit(train_images, train_labels, epochs=5, 
+                    validation_data=(test_images, test_labels))
+
+
+# ### Evaluate the model
+
+# In[11]:
+
+
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0.5, 1])
+plt.legend(loc='lower right')
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+
+# In[12]:
+
+
+print(test_acc)
+
+
+# Our simple CNN has achieved a test accuracy of over 70%. Not bad for a few lines of code! For another CNN style, see an example using the Keras subclassing API and a `tf.GradientTape` [here](https://www.tensorflow.org/tutorials/quickstart/advanced).
+
+# # Making predictions
+# from https://www.tensorflow.org/tutorials/keras/classification
+
+# In[16]:
+
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# ## Make predictions
+# 
+# With the model trained, you can use it to make predictions about some images.
+
+# In[13]:
+
+
+predictions = model.predict(test_images)
+
+
+# Here, the model has predicted the label for each image in the testing set. Let's take a look at the first prediction:
+
+# In[14]:
+
+
+predictions[0]
+
+
+# A prediction is an array of 10 numbers. They represent the model's "confidence" that the image corresponds to each of the 10 different articles of clothing. You can see which label has the highest confidence value:
+
+# In[17]:
+
+
+np.argmax(predictions[0])
+
+
+# So, the model is most confident that this image is an ankle boot, or `class_names[9]`. Examining the test label shows that this classification is correct:
+
+# In[18]:
+
+
+test_labels[0]
+
+
+# Graph this to look at the full set of 10 class predictions.
+
+# In[19]:
+
+
+def plot_image(i, predictions_array, true_label, img):
+  predictions_array, true_label, img = predictions_array, true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+
+  plt.imshow(img, cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  predictions_array, true_label = predictions_array, true_label[i]
+  plt.grid(False)
+  plt.xticks(range(10))
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array)
+
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
+
+
+# Let's look at the 0th image, predictions, and prediction array. Correct prediction labels are blue and incorrect prediction labels are red. The number gives the percentage (out of 100) for the predicted label.
+
+# In[24]:
+
+
+i = 0
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions[i], test_labels.reshape(-1), test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions[i],  test_labels.reshape(-1))
+plt.show()
+
+
+# In[25]:
+
+
+i = 12
+plt.figure(figsize=(6,3))
+plt.subplot(1,2,1)
+plot_image(i, predictions[i], test_labels.reshape(-1), test_images)
+plt.subplot(1,2,2)
+plot_value_array(i, predictions[i],  test_labels.reshape(-1))
+plt.show()
+
+
+# Let's plot several images with their predictions. Note that the model can be wrong even when very confident.
+
+# In[26]:
+
+
+# Plot the first X test images, their predicted labels, and the true labels.
+# Color correct predictions in blue and incorrect predictions in red.
+num_rows = 5
+num_cols = 3
+num_images = num_rows*num_cols
+plt.figure(figsize=(2*2*num_cols, 2*num_rows))
+for i in range(num_images):
+  plt.subplot(num_rows, 2*num_cols, 2*i+1)
+  plot_image(i, predictions[i], test_labels.reshape(-1), test_images)
+  plt.subplot(num_rows, 2*num_cols, 2*i+2)
+  plot_value_array(i, predictions[i], test_labels.reshape(-1))
+plt.tight_layout()
+
+
+# In[ ]:
+
+
+
+
